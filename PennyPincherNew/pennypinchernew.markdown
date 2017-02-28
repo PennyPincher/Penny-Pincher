a33b410a4a78633f52664c39c54e7ece6993405c
Hunter Kern <Hunterkern2015@u.northwestern.edu>
2017-02-27 21:05:38 -0600

    Fixed Chart

--- PennyPincherNew/pennypinchernew.markdown
+++
@@ -1,7 +0,0 @@
-PennyPincherNew
----------------
-
-
-A [Pen](http://codepen.io/suezq501/pen/egbgoe) by [Sue](http://codepen.io/suezq501) on [CodePen](http://codepen.io/).
-
-[License](http://codepen.io/suezq501/pen/egbgoe/license).
--- js/SummaryChart.js
+++ js/SummaryChart.js
@@ -1,5 +1,6 @@
 google.load('visualization', '1', { packages: ['corechart'] });
 google.setOnLoadCallback(drawChart1);
+google.setOnLoadCallback(drawChart2);
 
 function drawChart1() {
 	var data = new google.visualization.DataTable();
@@ -36,6 +37,41 @@ function drawChart1() {
 	chart.draw(data, options);
 }
 
+function drawChart2() {
+	var data = new google.visualization.DataTable();
+	data.addColumn('date', 'Date');
+	data.addColumn('number', 'Money Spent');
+
+	data.addRows([
+		[new Date('2017-01-01'), 10],
+		[new Date('2017-01-04'), 30],
+		[new Date('2017-01-07'), 40],
+		[new Date('2017-01-10'), 60],
+		[new Date('2017-01-13'), 40],
+		[new Date('2017-01-16'), 18],
+		[new Date('2017-01-19'), 20],
+		[new Date('2017-01-22'), 60],
+		[new Date('2017-01-25'), 40],
+		[new Date('2017-01-28'), 20],
+		[new Date('2017-01-31'), 25]
+	]);
+
+	var dateTicks = [];
+	for (var m = 01; m <= 30; m+=3)
+		dateTicks.push(new Date('2017-' + '01-' + m));
+
+	var options = {
+		hAxis: {
+			format: 'MMM d',
+			//title: 'Date'},
+			ticks: dateTicks},
+			legend: 'none',
+	};
+
+	var chart = new google.visualization.LineChart(document.getElementById('chart_div2'));
+	chart.draw(data, options);
+}
 $(window).resize(function(){
   drawChart1();
+  drawChart2();
 });
